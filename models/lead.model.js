5585b53114d5284c7af232ade67e8a95aa4d3c38
Jagadish Pundir <146165322+JD-3002@users.noreply.github.com>
2025-11-15 18:12:00 +0530

    timestamps added for lead

--- models/lead.model.js
+++ models/lead.model.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-const LeadSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName:  { type: String, required: true },
-  mobile:    { type: String, required: true },
-  email:     { type: String, required: true },
-  propertyIntent: {
-    type: String,
-    enum: ['sell', 'rent', 'both'],
-    required: true,
+const LeadSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName:  { type: String, required: true },
+    mobile:    { type: String, required: true },
+    email:     { type: String, required: true },
+    propertyIntent: {
+      type: String,
+      enum: ['sell', 'rent', 'both'],
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Lead', LeadSchema);
